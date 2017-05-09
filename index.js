2e1e52595249d7db89ea934312f2a1ec7a853a71
Thierry Nowak <thinow@gmail.com>
2017-05-09 22:40:20 +0200

    use google translation

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-const fn = text => `Hello ${text} !`;
+import assert from 'assert';
+import request from 'request';
 
-console.log(fn('World'));
+assert(process.env.API_KEY, 'process.env.API_KEY is mandatory');
+
+const API_KEY = process.env.API_KEY;
+const uri = `https://translation.googleapis.com/language/translate/v2?key=${API_KEY}`;
+
+const data = { q: 'machen', source: 'de', target: 'en' };
+
+request({ method: 'POST', uri, data }, (error, response, body) => {
+  assert(!error, `Error during the request : ${error}`);
+
+  // console.log('Response:', response);
+  console.log('Body:', body);
+});
