03eeb1d606d392afd340a6d4dac7fcb46c8dfe39
Thierry Nowak <thinow@gmail.com>
2017-05-10 22:45:35 +0200

    use cheerio

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 import assert from 'assert';
 import request from 'request';
-import xpath from 'xpath';
-import { DOMParser as dom } from 'xmldom';
+import cheerio from 'cheerio';
 
 const uri = `https://www.scholingua.com/en/de/conjugation/vergessen`;
 
 request(uri, (error, response, body) => {
   assert(!error, `Error during the request : ${error}`);
 
-  var doc = new dom().parseFromString(body);
-  const found = xpath.select('//title', doc);
-  found.map(item => console.log('XPath:', item.firstChild.data));
+  const $ = cheerio.load(body);
+
+  //*[@id="konjugationstabellen"]/div/div[1]/div[2]/table/tbody/tr[6]/td[2]/div/div[2]
+
+  const found = $('#konjugationstabellen')
+    .children('div') // k_tabelle
+    .children('div').eq(0) // block
+    .children('div').eq(1)
+    .children('table')
+    .children('tr').eq(5)
+    .children('td').eq(1)
+    .children('div')
+    .children('div').eq(1)
+    .text();
+  console.log(found);
 });
