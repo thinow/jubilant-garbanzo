c678d915414694aeb4486d917222504b524f5548
Thierry Nowak <thinow@gmail.com>
2017-05-14 23:07:01 +0200

    excel to json

---
+++ excel-to-json.js
@@ -0,0 +1,32 @@
+import fs from 'fs';
+import csv from 'csv';
+import assert from 'assert';
+
+const OUTPUT_FILE = './output/import.json';
+
+const [,,filename] = process.argv;
+console.log('FILE:', filename);
+
+const input = fs.readFileSync(filename);
+
+csv.parse(input, {}, (error, output) => {
+  assert.ok(!error, error);
+
+  const objects = output.map(([, category, label, translation, right, ...wrong]) => {
+    return { category, label, translation, answers: { right, wrong } };
+  });
+
+  const lines = objects
+    .map(JSON.stringify)
+    .map(line => line.concat('\n'));
+
+  try {
+    fs.unlinkSync(OUTPUT_FILE);
+  } catch (e) {
+  }
+
+  lines.forEach(line => {
+    fs.appendFileSync(OUTPUT_FILE, line, { encoding: 'utf8' });
+  });
+
+});
